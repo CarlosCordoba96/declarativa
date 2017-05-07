100768cbf34b23cbed3112adeb67298a443c2be8
Carlos Córdoba Ruiz <carlos.cordoba@alu.uclm.es>
2017-05-07 17:00:12 +0200

    Update gram.pl

--- gram.pl
+++ gram.pl
@@ -1,7 +1,13 @@
-s --> a(N), b(N), c(N).
-a(N) --> [a], a(N1), {N is N1 + 1}.
-a(0) --> [].
-b(N) --> [b], b(N1), {N is N1 + 1}.
-b(0) --> [].
-c(N) --> [c], c(N1), {N is N1 + 1}.
-c(0) --> [].
+oracion --> s_nominal(G, N), s_verbal(G, N).
+oracion --> s_verbal(_, _).
+
+s_nominal(Genero, Numero) --> determinante(Genero, Numero), s_nominal2(Genero, Numero).
+s_nominal(Genero, Numero) --> s_nominal2(Genero, Numero).
+s_nominal2(G, N) --> sustantivo(G, N).
+s_nominal2(G, N) --> s_adjetivo(G, N),  s_nominal(G, N).
+s_nominal2(G, N) --> sustantivo(G, N), s_adjetivo(G, N).
+
+s_adjetivo(G, N) --> adjetivo(G, N).
+s_adjetivo(G, N) --> s_adverbial, s_adjetivo(G, N).
+s_adverbial --> adverbio.
+s_verbal(G, N) --> verbo(copulativo, _, G, N, _), s_adjetivo(G, N).
