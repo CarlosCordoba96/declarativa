2d0052ac0386b5aeec6294125595195c46dc548e
Carlos Córdoba Ruiz <carlos.cordoba@alu.uclm.es>
2017-05-18 22:13:13 +0200

    Add files via upload

---
+++ Haskell/ArandaGarciaCordobaRuiz.hs
@@ -0,0 +1,15 @@
+qsort [] = []
+qsort (x:xs) = qsort menor ++ [x] ++ qsort mayor
+    where menor  = filter (< x) xs
+          mayor = filter (>= x) xs
+
+
+mi_map func [] = []
+mi_map func (x:xs) = func x:(mi_map func xs)
+
+suma func (x) = suma_aux func 0 x
+suma_aux func acum [] = acum
+suma_aux func acum (x:xs) = suma_aux func suma_parcial xs
+			    where suma_parcial = (func x) + acum
+
+suma' func (x) = foldl (\x y -> x + y) 0 (map func x)
